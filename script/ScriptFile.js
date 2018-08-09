470b8f64021ee72e10f6f8d1412af92b3d2cf9b7
Austin Stephens <26912293+akasos@users.noreply.github.com>
2018-08-09 00:14:13 -0600

    Added smooth scrolling and work-container sliders via jquery

--- script/ScriptFile.js
+++ script/ScriptFile.js
@@ -11,14 +11,14 @@ function sideNav()
         //Opening and closing the side nav. 
         if(window.innerWidth <= 768)
         {
+            //open up side nav on = MENU click.
             $('.toggleNav').on('click', function(){
-    
                 $('#mySidenav').css('width', '250px');
                 $('.landing-page').css("opacity", "0.5");
                 navOpen = true;
-    
             });
-    
+
+            //Close side nav if X is clicked.
             $('.closebtn').on('click', function(){
     
                 $('#mySidenav').css('width', "0px");
@@ -45,9 +45,77 @@ function sideNav()
             $('#mySidenav').css('width', '250px');
             $('.landing-page').css("opacity", "0.5");
         
-        }
-        
+        }   
 }
 
+
+/* Smooth scrolling */ 
+$('a[href^="#"]').on('click', function(event) {
+
+    //Prevent = MENU from throwing an air when toggle nav is click because of #.
+    if(event.currentTarget.className == "toggleNav")
+        return;
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+   
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1500);
+
+        //If the user clicks link with side nav, make sure to close it.
+        if(navOpen){
+        $('#mySidenav').css('width', "0px");
+        $('.landing-page').css("opacity", "1");
+         navOpen = false;
+        }
+        
+
+    }
 });
 
+ /* Sliding in work containers */
+
+ //Only registers a few scroll events at a time.
+function debounce(func, wait = 15, immediate = true) {
+        var timeout;
+        return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+        };
+    }
+
+const slideInWorkExp = document.querySelectorAll('.work-container');
+
+function checkSlide(e) {
+    
+        slideInWorkExp.forEach(workExp => {
+            //half way through the div
+            const slideInAt = (window.innerHeight + window.scrollY) - workExp.clientHeight / 2;
+            //bottom of the div;
+            const workBottom = workExp.offsetTop + workExp.clientHeight;
+            const isHalfShown = slideInAt > workExp.offsetTop;
+            const isNotScrollPast = window.scrollY < workBottom;
+            if(isHalfShown && isNotScrollPast)
+            {
+                workExp.classList.add('active');
+            }
+            else{
+                workExp.classList.remove('active');
+            }
+        });
+    }
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+
+});
