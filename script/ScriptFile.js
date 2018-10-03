bafac358ddffecbf6462a651daf4eb0dbbca3380
Austin Stephens <26912293+akasos@users.noreply.github.com>
2018-10-03 00:02:31 -0600

    Added google maps and contact information

--- script/ScriptFile.js
+++ script/ScriptFile.js
@@ -105,27 +105,16 @@ $(document).ready(function()
    
    }
 
-   /*Side margins for about and project containers */
-//    $(window).resize()
-//    let intro_image = document.querySelector('.intro-image');
-//    let about_container = document.querySelector('.about-container');
-//    let portfolio_container = document.querySelector('.portfolio-container');
-//    sideMargin();
-//    $(window).resize(sideMargin);
-//    function sideMargin()
-//    {
-//      console.log("what");
-//      if(window.innerWidth <= intro_image.offsetWidth)
-//     {
-//         about_container.style.margin = "25px 20px 0px 20px";
-//         portfolio_container.style.margin = "25px 20px 0px 20px";
-//     }
 
-//     else if(window.innerWidth > intro_image.offsetWidth)
-//     {
-//         about_container.style.margin = "25px 0px 0px 0px";
-//         portfolio_container.style.margin = "25px 0px 0px 0px";
-//     }
+//    /* Google Maps */
+//    function myMap() {
+
+//     let myCenter = new google.maps.LatLng(41.222759, -111.970421);
+//     let mapCanvas = document.getElementById('googleMap');
+//     let mapOptions = {center: myCenter, zoom: 5};
+//     let map = new google.maps.Map(mapCanvas, mapOptions);
+//     var marker = new google.maps.Marker({position: myCenter});
+
 //    }
   
 });
